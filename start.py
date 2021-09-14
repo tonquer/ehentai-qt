1cf1bf9876e0a37910221f2f8fb1fc3eef709e31
tonquer <oyt96@qq.com>
2021-09-14 10:48:50 +0800

    fix dpi error

--- start.py
+++ start.py
@@ -29,7 +29,7 @@ from src.util import Log
 
 
 if __name__ == "__main__":
-    QtWidgets.QApplication.setAttribute(Qt.AA_EnableHighDpiScaling)
+    # QtWidgets.QApplication.setAttribute(Qt.AA_EnableHighDpiScaling)
     Log.Init()
     Log.UpdateLoggingLevel()
     app = QtWidgets.QApplication(sys.argv)  # 建立application对象
